3c5d256a7ab82c9f119c2f462f1bf18c83b9a737
Aline Sanches <96800792+alnsanches@users.noreply.github.com>
2022-10-28 22:37:53 -0300

    projeto finalizado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake  = [
+    {
+        "id": "1",
+        "title": "Happy Halloween",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempus augue at justo consequat tempor. Sed ultrices, odio sed semper hendrerit, mauris felis semper lectus, et ultricies augue orci in arcu. Pellentesque molestie tincidunt sapien, sagittis vestibulum eros dapibus vehicula. Suspendisse interdum tempor dui at congue. Nunc laoreet ornare pharetra. Sed condimentum mi non volutpat laoreet. Cras feugiat, mauris vitae efficitur cursus, ante ligula convallis augue, at interdum nisi diam vitae ipsum. Nulla in orci ac elit elementum maximus. Nam vel faucibus leo.",
+        "photoCover": "https://images.unsplash.com/photo-1635414981532-e1926c95018a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA1fHxoYWxsb3dlZW58ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
+    },
+    {
+        "id": "2",
+        "title": "Participe do concurso de abóboras",
+        "description": "bla bla bla",
+        "photoCover": "https://images.unsplash.com/photo-1602814736315-13fe19927459?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+        "id": "3",
+        "title": "Vote na melhor decoração",
+        "description": "bla bla bla",
+        "photoCover": "https://images.unsplash.com/photo-1509557965875-b88c97052f0e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+    },
+    {
+        "id": "4",
+        "title": "Deliciosas receitas para a sua festa de Halloween",
+        "description": "bla bla bla",
+        "photoCover": "https://images.unsplash.com/photo-1603320284194-db4276c54f0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aGFsbG93ZWVuJTIwY2FuZHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+    }
+
+]
